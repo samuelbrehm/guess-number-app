0e0021a76c08e4a21075b164c1e9700bd2f7cd12
Samuel Brehm <samuelbrehm@gmail.com>
2019-10-23 10:07:29 -0300

    Created screen page

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,9 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
 
-const Header = props => { 
+import Colors from "../constants/colors";
+
+const Header = props => {
   return (
     <View style={styles.header}>
       <Text style={styles.headerTitle}>{props.title}</Text>
@@ -11,15 +13,15 @@ const Header = props => {
 
 const styles = StyleSheet.create({
   header: {
-    width: '100%',
+    width: "100%",
     height: 90,
     paddingTop: 36,
-    backgroundColor: '#f7287b',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: Colors.primary,
+    alignItems: "center",
+    justifyContent: "center"
   },
   headerTitle: {
-    color: 'black',
+    color: "black",
     fontSize: 18
   }
 });
